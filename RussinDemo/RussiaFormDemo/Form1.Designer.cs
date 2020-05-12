6b4fa1a8518435057c74f449a1aa9ece520d0602
lwt <973055363@qq.com>
2020-05-12 18:04:25 +0800

    下班了，明天再继续

--- RussinDemo/RussiaFormDemo/Form1.Designer.cs
+++ RussinDemo/RussiaFormDemo/Form1.Designer.cs
@@ -28,20 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblRowCount = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(433, 382);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(433, 431);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "暂停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(365, 443);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.WindowText;
+            this.panel2.Controls.Add(this.lblScore);
+            this.panel2.Controls.Add(this.lblRowCount);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(387, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(214, 341);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.WindowText;
+            this.panel3.Location = new System.Drawing.Point(20, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(169, 157);
+            this.panel3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(18, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "行数：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(18, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "分数：";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblScore.Location = new System.Drawing.Point(107, 235);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(22, 21);
+            this.lblScore.TabIndex = 8;
+            this.lblScore.Text = "0";
+            // 
+            // lblRowCount
+            // 
+            this.lblRowCount.AutoSize = true;
+            this.lblRowCount.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblRowCount.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblRowCount.Location = new System.Drawing.Point(107, 197);
+            this.lblRowCount.Name = "lblRowCount";
+            this.lblRowCount.Size = new System.Drawing.Size(22, 21);
+            this.lblRowCount.TabIndex = 7;
+            this.lblRowCount.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 475);
+            this.ClientSize = new System.Drawing.Size(613, 484);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblRowCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
---
+++ RussinDemo/RussiaFormDemo/Russia.cs
@@ -0,0 +1,120 @@
+﻿namespace RussiaFormDemo
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    class Russia
+    {
+        public Point firstPoi = new Point(140, 20);//定义方块的起始位置
+        public static Color[,] PlaceColor;//
+        public static bool[,] Place;//
+        public static int conWidth = 0;//记录列数
+        public static int conHeight = 0;//记录行数
+        public static int maxY = 0;//方块在行中的最小高度
+        public static int conMax = 0;//方块落下后的最大位置
+        public static int conMin = 0;//方块落下后的最小位置
+        bool[] tem_Array = { false, false, false, false };//记录方块组中那一块所在行中已满
+        Color ConColor = Color.Coral;
+        Point[] ArryPoi = new Point[4];//方块的数组
+        Point[] Arryfront = new Point[4];//前一个方块的数组
+        int Cake = 20;//定义方块的大小
+        int Convertor = 0;//变换器
+        Control Mycontrol = new Control();//实例化Control
+        public Label Label_Linage = new Label();//实例化Label，用于显示去除的行数
+        public Label Label_Fraction = new Label();//实例化Label，用于显示分数
+        public static int[] ArrayCent = new int[] { 2, 5, 9, 15 };//记录加分情况
+        public Timer timer = new Timer();
+
+        public void CakeMode(int n)
+        {
+            ArryPoi[0] = firstPoi;//记录方块起始位置
+            switch (n)
+            {
+                case 1://:L方块
+                    ArryPoi[1] = new Point(ArryPoi[0].X, ArryPoi[0].Y - Cake);
+                    ArryPoi[2] = new Point(ArryPoi[0].X, ArryPoi[0].Y -2* Cake);
+                    ArryPoi[3] = new Point(ArryPoi[0].X+Cake, ArryPoi[0].Y);
+                    ConColor = Color.Fuchsia;//设置当前方块颜色
+                    Convertor = 2;//方块的变换样式
+                    break;
+                case 2://组合“Z”方块
+                     ArryPoi[1] = new Point(firstPoi.X, firstPoi.Y - Cake);
+                     ArryPoi[2] = new Point(firstPoi.X - Cake, firstPoi.Y - Cake);
+                     ArryPoi[3] = new Point(firstPoi.X + Cake, firstPoi.Y);
+                     ConColor = Color.Yellow;
+                     Convertor = 6;
+                     break;
+                         
+                case 3://组合倒“L”方块
+                     ArryPoi[1] = new Point(firstPoi.X, firstPoi.Y - Cake);
+                     ArryPoi[2] = new Point(firstPoi.X, firstPoi.Y + Cake);
+                     ArryPoi[3] = new Point(firstPoi.X - Cake, firstPoi.Y + Cake);
+                     ConColor = Color.CornflowerBlue;
+                     Convertor = 8;
+                     break;
+                case 4://组合倒“Z”方块
+                     ArryPoi[1] = new Point(firstPoi.X, firstPoi.Y - Cake);
+                     ArryPoi[2] = new Point(firstPoi.X + Cake, firstPoi.Y - Cake);
+                     ArryPoi[3] = new Point(firstPoi.X - Cake, firstPoi.Y);
+                     ConColor = Color.Blue;
+                     Convertor = 12;
+                     break;
+                case 5://组合“T”方块
+                     ArryPoi[1] = new Point(firstPoi.X, firstPoi.Y - Cake);
+                     ArryPoi[2] = new Point(firstPoi.X + Cake, firstPoi.Y - Cake);
+                     ArryPoi[3] = new Point(firstPoi.X - Cake, firstPoi.Y - Cake);
+                     ConColor = Color.Silver;
+                     Convertor = 14;
+                     break;
+                case 6://组合“一”方块
+                     ArryPoi[1] = new Point(firstPoi.X + Cake, firstPoi.Y);
+                     ArryPoi[2] = new Point(firstPoi.X - Cake, firstPoi.Y);
+                     ArryPoi[3] = new Point(firstPoi.X - Cake * 2, firstPoi.Y);
+                     ConColor = Color.Red;
+                     Convertor = 18;
+                     break;
+                case 7://组合“田”方块
+                     ArryPoi[1] = new Point(firstPoi.X - Cake, firstPoi.Y);
+                     ArryPoi[2] = new Point(firstPoi.X - Cake, firstPoi.Y - Cake);
+                     ArryPoi[3] = new Point(firstPoi.X, firstPoi.Y - Cake);
+                     ConColor = Color.LightGreen;
+                     Convertor = 19;
+                     break;
+                default:
+                    break;
+            }
+        }
+
+        public void ConvertorClear()
+        {
+            if(Mycontrol!=null)
+            {
+                Graphics g = Mycontrol.CreateGraphics();
+                Rectangle rect = new Rectangle(0, 0, Mycontrol.Width, Mycontrol.Height);
+                MyPaint(g, new SolidBrush(Color.Black), rect);
+            }
+        }
+
+        public void MyPaint(Graphics g,Brush colorBrush,Rectangle rect)
+        {
+            g.FillRectangle(colorBrush, rect);
+        }
+        //1,随机产生方块
+        public void CreateCake()
+        {
+            Random rd = new Random();
+            int n = rd.Next();
+        }
+    }
+    //生成小方块
+    public class SmallCake
+    {
+        public Point point { get; set; }
+        public const int cake = 20;
+        public SmallCake(Graphics g)
+        {
+
+        }
+    }
+}
